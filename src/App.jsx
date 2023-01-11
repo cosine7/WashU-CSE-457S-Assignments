e8bee36fc7a96684bf9eb8ed12410eec4da73b2c
ChenXiao Liu <810196813@qq.com>
2023-01-11 14:07:26 -0600

    complete legend

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import YearSelector from './components/YearSelector';
 import ElectoralVoteChart from './components/ElectoralVoteChart';
 import VotePercentageChart from './components/VotePercentageChart';
+import Legend from './components/Legend';
 
 export default function App() {
   return (
     <>
       <h1>US Presidential Elections from 1940 to 2016</h1>
       <YearSelector />
+      <Legend />
       <ElectoralVoteChart />
       <VotePercentageChart />
     </>
---
+++ src/components/Legend/index.jsx
@@ -0,0 +1,41 @@
+import { domain, range } from '../../util/scaleColor';
+
+const margin = 50;
+const groupWidth = window.innerWidth - margin * 2;
+const width = groupWidth / range.length;
+
+export default function Legend() {
+  return (
+    <svg height={margin * 2} width={window.innerWidth}>
+      <g transform={`translate(${margin})`}>
+        {range.map((color, i) => (
+          <rect
+            key={color}
+            width={width}
+            height={margin / 2}
+            x={width * i}
+            y={0}
+            fill={color}
+            stroke="white"
+          />
+        ))}
+      </g>
+      <g transform={`translate(${margin}, ${margin * 0.7})`}>
+        {range.map((color, i) => (
+          <text
+            key={color}
+            x={width * i + width / 2}
+            y={0}
+            textAnchor="middle"
+          >
+            {domain[i]}
+            {' '}
+            to
+            {' '}
+            {domain[i + 1]}
+          </text>
+        ))}
+      </g>
+    </svg>
+  );
+}
