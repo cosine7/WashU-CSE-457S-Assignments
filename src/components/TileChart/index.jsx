7a2b11736ed3c36ed74db37ef11256f482db93fa
ChenXiao Liu <810196813@qq.com>
2023-01-13 14:15:07 -0600

    complete tile chart color scale

--- src/components/TileChart/index.jsx
+++ src/components/TileChart/index.jsx
@@ -1,7 +1,9 @@
 import {
   geoPath, geoAlbersUsa,
 } from 'd3';
+import { useSelector } from 'react-redux';
 import usStates from '../../assets/data/states.geo.json';
+import scaleColor from '../../util/scaleColor';
 
 const margin = 50;
 const groupWidth = window.innerWidth * 0.7 - margin * 2;
@@ -9,8 +11,23 @@ const projection = geoAlbersUsa().fitWidth(groupWidth, usStates);
 
 const generatePath = geoPath(projection);
 
+const getColor = state => {
+  if (!state) {
+    return 'white';
+  }
+  return scaleColor(state.party, state.victory);
+};
+
 console.log(usStates.features);
 export default function TileChart() {
+  const states = useSelector(state => state.yearSelector.data.reduce((previous, current) => {
+    previous[current.abbreviation] = {
+      party: current.party,
+      victory: current.victory,
+    };
+    return previous;
+  }, {}));
+  console.log(states);
   return (
     <svg height={window.innerWidth * 0.5} width={window.innerWidth}>
       <g transform={`translate(${margin})`}>
@@ -18,6 +35,9 @@ export default function TileChart() {
           <path
             key={state.id}
             d={generatePath(state)}
+            fill={getColor(states[state.properties.STUSPS])}
+            stroke="white"
+            cursor="pointer"
           />
         ))}
       </g>
