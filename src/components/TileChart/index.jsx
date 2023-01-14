a1049e069c82db51af4aecb3dd8d7644aafc4e70
ChenXiao Liu <810196813@qq.com>
2023-01-14 11:13:55 -0600

    complete basic tooltip

--- src/components/TileChart/index.jsx
+++ src/components/TileChart/index.jsx
@@ -1,9 +1,8 @@
-import {
-  geoPath, geoAlbersUsa,
-} from 'd3';
+import { geoPath, geoAlbersUsa } from 'd3';
 import { useSelector } from 'react-redux';
 import usStates from '../../assets/data/states.geo.json';
 import scaleColor from '../../util/scaleColor';
+import Tooltip from '../Tooltip';
 
 const margin = 50;
 const groupWidth = window.innerWidth * 0.7 - margin * 2;
@@ -16,6 +15,18 @@ const getColor = state => {
     return 'white';
   }
   return scaleColor(state.party, state.victory);
+};
+
+const getTooltipContent = state => {
+  if (!state) {
+    return null;
+  }
+  return (
+    <div>
+      <p>{state.party}</p>
+      <p>{state.victory}</p>
+    </div>
+  );
 };
 
 console.log(usStates.features);
@@ -32,13 +43,14 @@ export default function TileChart() {
     <svg height={window.innerWidth * 0.5} width={window.innerWidth}>
       <g transform={`translate(${margin})`}>
         {usStates.features.map(state => (
-          <path
-            key={state.id}
-            d={generatePath(state)}
-            fill={getColor(states[state.properties.STUSPS])}
-            stroke="white"
-            cursor="pointer"
-          />
+          <Tooltip key={state.id} content={getTooltipContent(states[state.properties.STUSPS])}>
+            <path
+              d={generatePath(state)}
+              fill={getColor(states[state.properties.STUSPS])}
+              stroke="white"
+              cursor="pointer"
+            />
+          </Tooltip>
         ))}
       </g>
     </svg>
